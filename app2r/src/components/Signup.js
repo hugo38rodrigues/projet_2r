6fb46f5b1573c188e8587b5eeffe85512ee38bad
Hugo Rodrigues <hugo38.rodrigues@gmail.com>
2022-03-03 10:07:00 +0100

    ajout post

--- app2r/src/components/Signup.js
+++ app2r/src/components/Signup.js
@@ -1,62 +1,92 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 const Signup = () => {
-  const [password, setPassword] = useState("");
+  const [psswrd, setPassword] = useState("");
   const [confirmPass, setConfirmPass] = useState("");
+  const [nom, setNom] = useState("");
+  const [prenom, setPrenom] = useState("");
+  const [date_naiss, setDate_naiss] = useState("");
+  const [email, setEmail] = useState("");
+  const [login, setLogin] = useState("");
+
+  const handleSubmit = (event) => {
+    const post = {
+      nom: nom,
+      prenom: prenom,
+      date_naiss: date_naiss,
+      email: email,
+      login: login,
+      psswrd: psswrd
+
+    };
+    event.preventDefault();
+
+    axios.post("adresse API", post)
+      .then(res => {
+
+        console.log("Status", res.status);
+        console.log("Data", res.data);
+        console.log(post)
+      })
+      .catch(({ error }) => {
+        console.error('erreur envois enregistrement', error);
+      });
+  }
 
   return (
 
     <div className="signup">
-      <form method="post">
+      <form onSubmit={handleSubmit}>
         <h2>Inscrivez-vous</h2>
 
         <label>Nom</label>
         <br />
-        <input type="text" required/>
+        <input type="text" value={nom} onChange={e => setNom(e.target.value)} required />
         <br />
         <br />
 
         <label>Prénom</label>
         <br />
-        <input type="text" required/>
+        <input type="text" value={prenom} onChange={e => setPrenom(e.target.value)} required />
         <br />
         <br />
 
-        <label>Pseudo</label>
+        <label>Login</label>
         <br />
 
-        <input type="text" required />
+        <input type="text" value={login} onChange={e => setLogin(e.target.value)} required />
         <br />
         <br />
 
         <label> Date de naissance </label>
-        <br/>
-        <input type="date" required></input>
-        <br/>
-        <br/>
+        <br />
+        <input type="date" value={date_naiss} onChange={e => setDate_naiss(e.target.value)} required></input>
+        <br />
+        <br />
 
         <label>Adresse mail</label>
-        <br/>
-        <input type="email" required></input>
+        <br />
+        <input type="email" value={email} onChange={e => setEmail(e.target.value)} required></input>
         <br />
         <br />
 
         <label>Mot de passe</label>
         <br />
         <input type="password" onChange={(e) => setPassword(e.target.value)} required />
-        {password.length < 8 && password.length>3&& (
+        {psswrd.length < 8 && psswrd.length > 3 && (
           <p>Le mot de passe est trop petit, il doit faire 8 caractères minimum</p>
         )}
         <br />
         <br />
         <label>Confirmer mot de passe</label>
         <br />
-        <input type="password" onChange={(e) => setConfirmPass(e.target.value)} required/>
-        {confirmPass.length > 4 && password !== confirmPass && (
+        <input type="password" onChange={(e) => setConfirmPass(e.target.value)} required />
+        {confirmPass.length > 4 && psswrd !== confirmPass && (
           <p>Les mots de passes ne correspondent pas</p>
         )}
-        <br/>
-        <br/>
+        <br />
+        <br />
 
         <button type="submit"> S'enregistrer</button>
       </form>
